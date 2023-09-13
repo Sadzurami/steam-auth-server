5d9569b199ba692e19124d0de870357d0b9f99aa
Sadzurami <sadzurami2036@gmail.com>
2023-09-13 16:03:39 +0300

    remove some useless info

--- src/filters/error.filter.ts
+++ src/filters/error.filter.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 
@@ -7,15 +7,8 @@ export class ErrorFilter implements ExceptionFilter {
   catch(exception: HttpException | Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
     const status = exception instanceof HttpException ? exception.getStatus() : 500;
 
-    response.status(status).send({
-      status: 'error',
-      statusCode: status,
-      message: exception.message,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+    response.status(status).send({ status: 'error', message: exception.message });
   }
 }
